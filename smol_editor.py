c49531cf38e8c6b74f3152d153f54c58afd1d82d
litotheam <zhaoliangli90@gmail.com>
2022-11-30 23:51:51 +0900

    create toplevel window and frame

--- smol_editor.py
+++ smol_editor.py
@@ -1,3 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="A very Smol Text Editor").grid(column=0, row=0)
+ttk.Button(frm, text="Leave", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
