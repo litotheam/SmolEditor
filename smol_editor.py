075256746605b6ffc141c72538f1c6dde955d453
litotheam <zhaoliangli90@gmail.com>
2022-12-01 23:08:41 +0900

    added tk Text widget as texting area

--- smol_editor.py
+++ smol_editor.py
@@ -4,6 +4,10 @@ from tkinter import ttk
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
+
+text_editor = Text(root)
+text_editor.grid(row=0, column=0, sticky="nsew")
+
 ttk.Label(frm, text="A very Smol Text Editor").grid(column=0, row=0)
 ttk.Button(frm, text="Leave", command=root.destroy).grid(column=1, row=0)
 root.mainloop()
