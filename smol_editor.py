4b5314332256dbdf6a5c9cbac58f57b1a040e5f1
litotheam <zhaoliangli90@gmail.com>
2022-12-01 23:12:30 +0900

    added a texting area using tk scrolledtext widget

--- smol_editor.py
+++ smol_editor.py
@@ -1,13 +1,17 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import scrolledtext
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 
-text_editor = Text(root)
-text_editor.grid(row=0, column=0, sticky="nsew")
+text_editor = Text(frm)
+text_editor.grid(row=1, column=0)
 
-ttk.Label(frm, text="A very Smol Text Editor").grid(column=0, row=0)
-ttk.Button(frm, text="Leave", command=root.destroy).grid(column=1, row=0)
+scrolled_text = scrolledtext.ScrolledText(frm)
+scrolled_text.grid(row=2, column=0)
+
+ttk.Label(frm, text="A very Smol Text Editor").grid(row=0, column=0)
+ttk.Button(frm, text="Leave", command=root.destroy).grid(row=0, column=1)
 root.mainloop()
